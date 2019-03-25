703c85867687140f203b46f2192dc8007cc5fe53
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-25 20:47:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,32 @@
 # Write your code here.
+#attendeesArray = []
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+  badgeList = []
+  attendees.each do |name|
+    badgeList.push(badge_maker(name))
+  end
+  badgeList
+end
+def assign_rooms(attendees)
+  roomAssignments = []
+  counter = 1
+  attendees.each do |name|
+    roomAssignments.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  roomAssignments
+end
+def printer(attendees)
+  batch_badge_creator(attendees).each do |name|
+    puts name
+  end
+  assign_rooms(attendees).each do |name|
+    puts name
+  end
 end
 
-puts badge_maker("nathan")
+
+puts assign_rooms(["nathan", "iqra", "ted"])
